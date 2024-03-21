12aa46f13d31f56bbe5032752dcdcfea17c974f0
Chad Freeman <ceo@marinadev.xyz>
2024-03-21 19:15:56 -0400

    set up simple deno server (routes soon)

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,41 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { Application, Context, Next, Router } from '@oak/oak'
+import { serveDir } from '@std/http'
+import { nerdIcons, rubiks, withDates } from '@rubiks/rubiks'
+
+const oak = new Application()
+
+const dev = !Deno.env.has('DENO_REGION')
+const dist = dev ? '../client/dist' : './dist'
+const logger = rubiks().use(nerdIcons()).use(withDates)
+
+async function client(ctx: Context, next: Next) {
+	// this code sucks but whatever it's going to have to do for now.
+	const req = ctx.request
+	const res = await serveDir(
+		new Request(req.url, {
+			headers: req.headers,
+		}),
+		{
+			fsRoot: dist,
+			showIndex: true,
+      urlRoot: "client"
+		},
+	)
+	if (res.status == 404) {
+		return next()
+	} else {
+    Array.from(res.headers.entries()).forEach(([key, value]) => ctx.response.headers.set(key, value))
+		ctx.response.body = res.body
+		ctx.response.status = res.status
+		ctx.respond = true
+	}
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+oak.use(client)
+
+logger.info('Starting web server.')
+
+oak.listen({
+	port: 8000,
+	hostname: '0.0.0.0',
+})
